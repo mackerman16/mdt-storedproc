16d4fe790c20ffd3dbdf9a71f0ff7836ad6ee23b
Matt-Ackerman <ackerman.j.matt@gmail.com>
2018-06-15 11:45:02 -0500

    Initial commit of the stored procedure used to set our master LastRunTimestamp.

---
+++ sp_LastRunTimestamp_Generator.sql
@@ -0,0 +1,15 @@
+
+/*
+ * Stored procedure that sets LastRunTimestamp to tomorrow at midnight.
+ */ 
+CREATE PROCEDURE dw.sp_LastRunTimestamp_Generator AS
+
+    -- Inserting the new run timestamps for the clients we just ran the procedure on.
+    DECLARE @updateLastRunTimestamp AS NVARCHAR(2000) = N'UPDATE [xAnalytics_DW].[dw].[LastRunTimestamp]
+                                                          SET MASTER_LastRunTimestamp = ' + Convert(NVARCHAR, DATEDIFF(DAY, 0, GETDATE())) + 
+                                                        N' WHERE ID = 1;'
+    EXECUTE(@updateLastRunTimestamp)
+
+GO
+
+EXEC dw.sp_LastRunTimestamp_Generator
